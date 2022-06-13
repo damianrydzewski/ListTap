5fd54e7425cf74398c582c15d4fe09579980d1ee
Damian Rydzewski <damianrydzewski.dev@gmail.com>
2022-06-13 15:07:20 +0200

    Initial Commit

---
+++ ListTap/ListTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTapApp.swift
+//  ListTap
+//
+//  Created by Damian on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
